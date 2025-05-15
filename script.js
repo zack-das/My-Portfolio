6e1629290a8545923aa14cf454f29e91c94e44ab
zack-das <izackdas@gmail.com>
2025-05-15 21:25:17 +0300

    updated css

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-const [protectedText, setProtectedText] = useState("");
 
-useEffect(() => {
-  fetch("/api/getText")
-    .then((res) => res.text())
-    .then((text) => setProtectedText(text));
-}, []);
